68277e3153d24e00dba6851f6e850ee74a82a6f1
lrsrivera <lr.srivera@mmdc.mcl.edu.ph>
2025-04-01 16:59:02 +0800

    Update urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -11,27 +11,33 @@ from .views import toggle_like_post
 from .views import toggle_follow, get_following_list
 
 urlpatterns = [
-    # User URLs
+   # User URLs
     path('users/', views.get_users, name='get_users'),
     path('users/create/', views.create_user, name='create_user'),
     path('users/<int:user_id>/verify/', views.verify_email, name='verify_email'),
     path('users/<int:user_id>/delete/', views.delete_user, name='delete_user'),
+    path('account/delete/', views.delete_account, name='delete_account'),
     path('users/<int:user_id>/details/', views.get_user_details, name='get_user_details'),
 
     # Post URLs
     path('posts/', views.get_posts, name='get_posts'),
+    path('posts/my/', views.get_my_posts, name='get_my_posts'),
     path('posts/create/', views.create_post, name='create_post'),
     path('posts/<int:post_id>/modify/', views.update_or_delete_post, name='update_or_delete_post'),
     path('posts/<int:post_id>/like/', views.toggle_like_post, name='toggle_like_post'),
+    path('posts/<int:post_id>/like/', toggle_like_post, name='toggle-like-post'),
+
 
     # Comments
     path('comments/create/', views.create_comment, name='create_comment'),
     path('comments/', views.get_comments, name='get_comments'),
-    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('comments/my/', views.get_my_comments, name='get_my_comments'),
+    path('posts/<int:post_id>/comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
     path('posts/<int:post_id>/comments/', views.get_comments, name='get_comments'),
     path('posts/<int:post_id>/comments/create/', views.create_comment, name='create_comment'),
-    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
-    
+    path('posts/<int:post_id>/comments/<int:comment_id>/toggle-like/', views.toggle_like_comment, name='toggle_like_comment'),
+    path('comments/all/', views.get_all_comments, name='get_all_comments'),
+
     # Token Authentication
     path('api/token/', CustomAuthToken.as_view(), name='api_token_auth'),
 
@@ -44,13 +50,16 @@ urlpatterns = [
     path('create-comment/', CreateCommentView.as_view(), name='create-comment'),
     
     path('auth/google/login/', GoogleLoginView.as_view(), name='google_login'),
-    path('accounts/', include('allauth.urls')), 
+    path('accounts/', include('allauth.urls')),  # Required for django-allauth
 
+    #Newsfeed
     path('feed/', NewsFeedView.as_view(), name='news-feed'),
-    path('posts/<int:post_id>/like/', toggle_like_post, name='toggle-like-post'),
+    path('posts/<int:post_id>/', views.get_post_detail, name='get_post_detail'),
+
+    # Follwers
+    path('users/<int:user_id>/followers/', views.get_followers_list, name='get_followers_list'),
+    path('users/<int:user_id>/following/', views.get_following_list, name='get_following_list'),
     path('users/<int:user_id>/follow/', toggle_follow, name='toggle-follow'),
-    path('users/<int:user_id>/following/', get_following_list, name='get-following-list'),
-    
-    
+
 
 ]
